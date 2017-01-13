c653cc62e5c940e0d88fe2436062c1f5d4bc2eb9
Zakor Gyula <zakorgy@inf.u-szeged.hu>
2017-01-13 10:27:50 +0100

    Basic Permissions API implementation

---
+++ components/script/dom/permissions.rs
@@ -0,0 +1,170 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use dom::bindings::codegen::Bindings::PermissionStatusBinding::PermissionDescriptor;
+use dom::bindings::codegen::Bindings::PermissionsBinding::{self, PermissionsMethods};
+use dom::bindings::error::Error;
+use dom::bindings::js::Root;
+use dom::bindings::reflector::{DomObject, Reflector, reflect_dom_object};
+use dom::globalscope::GlobalScope;
+use dom::permissionstatus::{PermissionDescriptorType, PermissionStatus};
+use dom::promise::Promise;
+use js::conversions::{ConversionResult, FromJSValConvertible};
+use js::jsapi::{JSContext, JSObject};
+use js::jsval::{ObjectValue, UndefinedValue};
+use std::rc::Rc;
+
+const CONVERSION_ERROR: &'static str = "Can't convert to an IDL value of type PermissionDescriptor";
+
+#[dom_struct]
+pub struct Permissions {
+    reflector_: Reflector,
+}
+
+impl Permissions {
+    pub fn new_inherited() -> Permissions {
+        Permissions {
+            reflector_: Reflector::new(),
+        }
+    }
+
+    pub fn new(global: &GlobalScope) -> Root<Permissions> {
+        reflect_dom_object(box Permissions::new_inherited(),
+                           global,
+                           PermissionsBinding::Wrap)
+    }
+}
+
+#[allow(unsafe_code)]
+fn create_root_descriptor(cx: *mut JSContext,
+                          permission_descriptor_obj: *mut JSObject)
+                          -> Result<PermissionDescriptor, String> {
+    rooted!(in(cx) let mut property = UndefinedValue());
+    property.handle_mut().set(ObjectValue(permission_descriptor_obj));
+    unsafe {
+        match PermissionDescriptor::from_jsval(cx, property.handle(), ()) {
+            Ok(ConversionResult::Success(descriptor)) => Ok(descriptor),
+            Ok(ConversionResult::Failure(message)) => Err(String::from(message)),
+            Err(_) => Err(String::from(CONVERSION_ERROR)),
+        }
+    }
+}
+
+// https://w3c.github.io/permissions/#dom-permissions-query
+fn sync_default_permission_query_call(global: &GlobalScope,
+                                      descriptor: PermissionDescriptor,
+                                      promise: &Rc<Promise>,
+                                      cx: *mut JSContext) {
+    // Step 5.
+    let status = PermissionStatus::create_from_descriptor(global, PermissionDescriptorType::Default(descriptor));
+    // Step 6.
+    // NOTE: `status.query` is the same as descriptor
+    status.permission_query(&*status.get_query().borrow());
+    // Step 7.
+    promise.resolve_native(cx, &status);
+}
+
+// https://w3c.github.io/permissions/#dom-permissions-request
+fn sync_default_permission_request_call(global: &GlobalScope,
+                                        descriptor: PermissionDescriptor,
+                                        promise: &Rc<Promise>,
+                                        cx: *mut JSContext) {
+    // Step 5.
+    let status = PermissionStatus::create_from_descriptor(global, PermissionDescriptorType::Default(descriptor));
+    // Step 6.
+    // NOTE: `status.query` is the same as descriptor
+    if let Err(err) = status.permission_request(&*status.get_query().borrow()) {
+        // Step 7.
+        promise.reject_error(cx, err);
+    }
+    // Step 8.
+    promise.resolve_native(cx, &status);
+}
+
+impl PermissionsMethods for Permissions {
+    #[allow(unrooted_must_root)]
+    #[allow(unsafe_code)]
+    // https://w3c.github.io/permissions/#dom-permissions-query
+    unsafe fn Query(&self, cx: *mut JSContext, permissionDesc: *mut JSObject) -> Rc<Promise> {
+        // Step 3.
+        let p = Promise::new(&self.global());
+
+        // Step 1.
+        let root_desc = match create_root_descriptor(cx, permissionDesc) {
+            Ok(descriptor) => descriptor,
+            Err(message) => {
+                p.reject_error(cx, Error::Type(message));
+                return p;
+            },
+        };
+
+        // Step 2.
+        // TODO: Add support for not default cases.
+        match root_desc.name {
+            _ => {
+                // Step 4: TODO: Add an async implementation instead of sync_default_permission_query_call
+                sync_default_permission_query_call(&self.global(), root_desc, &p, cx);
+            },
+        };
+
+        return p;
+    }
+
+    #[allow(unrooted_must_root)]
+    #[allow(unsafe_code)]
+    // https://w3c.github.io/permissions/#dom-permissions-request
+    unsafe fn Request(&self, cx: *mut JSContext, permissionDesc: *mut JSObject) -> Rc<Promise> {
+        // Step 3.
+        let p = Promise::new(&self.global());
+
+        // Step 1.
+        let root_desc = match create_root_descriptor(cx, permissionDesc) {
+            Ok(descriptor) => descriptor,
+            Err(message) => {
+                p.reject_error(cx, Error::Type(message));
+                return p;
+            },
+        };
+
+        // Step 2.
+        // TODO: Add support for not default cases.
+        match root_desc.name {
+            _ => {
+                // Step 4: TODO: Add an async implementation instead of sync_default_permission_request_call
+                sync_default_permission_request_call(&self.global(), root_desc, &p, cx);
+            },
+        };
+
+        return p;
+    }
+
+    #[allow(unrooted_must_root)]
+    #[allow(unsafe_code)]
+    // https://w3c.github.io/permissions/#dom-permissions-revoke
+    unsafe fn Revoke(&self, cx: *mut JSContext, permissionDesc: *mut JSObject) -> Rc<Promise> {
+        let p = Promise::new(&self.global());
+
+        // Step 1.
+        let root_desc = match create_root_descriptor(cx, permissionDesc) {
+            Ok(descriptor) => descriptor,
+            Err(message) => {
+                p.reject_error(cx, Error::Type(message));
+                return p;
+            },
+        };
+
+        // Step 2.
+        // TODO: Add support for not default cases.
+        match root_desc.name {
+            _ => {
+                // TODO: Clarify Step 3 - 4
+                // NOTE(zakorgy): The steps are not clear about that we should call here a permission revoke method
+                // but it is mentioned in `https://w3c.github.io/permissions/#permission-revocation-algorithm`:
+                // "Run by Permissions' revoke() method"
+            },
+        };
+        // Step 5.
+        return self.Query(cx, permissionDesc);
+    }
+}
---
+++ components/script/dom/permissionstatus.rs
@@ -0,0 +1,162 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use core::clone::Clone;
+use dom::bindings::cell::DOMRefCell;
+use dom::bindings::codegen::Bindings::EventHandlerBinding::EventHandlerNonNull;
+use dom::bindings::codegen::Bindings::PermissionStatusBinding::{self, PermissionDescriptor, PermissionName};
+use dom::bindings::codegen::Bindings::PermissionStatusBinding::{PermissionState, PermissionStatusMethods};
+use dom::bindings::error::ErrorResult;
+use dom::bindings::js::Root;
+use dom::bindings::reflector::{DomObject, reflect_dom_object};
+use dom::bindings::trace::JSTraceable;
+use dom::eventtarget::EventTarget;
+use dom::globalscope::GlobalScope;
+use heapsize::HeapSizeOf;
+use js::jsapi::{JS_GetIsSecureContext, JSObject, JSTracer};
+use js::rust::get_object_compartment;
+
+// Enum for storing different type of PermissionDescriptors in the same type.
+#[derive(JSTraceable, HeapSizeOf)]
+pub enum PermissionDescriptorType {
+    // TODO(zakorgy): Finish this list.
+    Undefined,
+    Default(PermissionDescriptor),
+}
+
+#[dom_struct]
+pub struct PermissionStatus {
+    eventtarget: EventTarget,
+    state: DOMRefCell<PermissionState>,
+    query: DOMRefCell<PermissionDescriptorType>,
+}
+
+impl PermissionStatus {
+    pub fn new_inherited() -> PermissionStatus {
+        PermissionStatus {
+            eventtarget: EventTarget::new_inherited(),
+            state: DOMRefCell::new(PermissionState::Denied),
+            query: DOMRefCell::new(PermissionDescriptorType::Undefined),
+        }
+    }
+
+    pub fn new(global: &GlobalScope) -> Root<PermissionStatus> {
+        reflect_dom_object(box PermissionStatus::new_inherited(),
+                           global,
+                           PermissionStatusBinding::Wrap)
+    }
+
+    fn set_query(&self, permission_descriptor: PermissionDescriptorType) {
+        *self.query.borrow_mut() = permission_descriptor;
+    }
+
+    pub fn get_query(&self) -> &DOMRefCell<PermissionDescriptorType> {
+        &self.query
+    }
+
+    // https://w3c.github.io/permissions/#create-a-permissionstatus
+    pub fn create_from_descriptor(global: &GlobalScope,
+                                  permission_desc: PermissionDescriptorType)
+                                  -> Root<PermissionStatus> {
+        let permission_status = PermissionStatus::new(global);
+        permission_status.set_query(permission_desc);
+        permission_status
+    }
+
+    // https://w3c.github.io/permissions/#boolean-permission-query-algorithm
+    pub fn permission_query(&self, permission_desc: &PermissionDescriptorType) {
+        // Step 1.
+        *self.state.borrow_mut() =
+            get_descriptors_permission_state(permission_desc, self.eventtarget.reflector().get_jsobject().get());
+    }
+
+    // https://w3c.github.io/permissions/#boolean-permission-request-algorithm
+    pub fn permission_request(&self, permission_desc: &PermissionDescriptorType) -> ErrorResult {
+        // Step 1.
+        self.permission_query(permission_desc);
+        // Step 2.
+        match *self.state.borrow() {
+            PermissionState::Prompt => {
+                // TODO: Step 3: Ask the users's permission.
+                // https://w3c.github.io/permissions/#request-permission-to-use
+
+                // Step 4.
+                return Ok(self.permission_query(permission_desc));
+            },
+            _ => {
+                return Ok(());
+            },
+        }
+    }
+}
+
+#[allow(unsafe_code)]
+// https://w3c.github.io/permissions/#permission-state
+fn get_descriptors_permission_state(descriptor: &PermissionDescriptorType, obj: *mut JSObject) -> PermissionState {
+    // TODO: Step 1.
+
+    let name = match descriptor {
+        // TODO(zakorgy): Finish this list.
+        &PermissionDescriptorType::Default(ref desc) => desc.name,
+        &PermissionDescriptorType::Undefined => return PermissionState::Denied,
+    };
+
+    unsafe {
+        // Step 2.
+        if !(JS_GetIsSecureContext(get_object_compartment(obj)) || allowed_in_nonsecure_contexts(name)) {
+            return PermissionState::Denied;
+        }
+    }
+    // TODO: Step 3: Store the invocation results
+    // TODO: Step 4: Make interaction with the user to discover the user's intent.
+    PermissionState::Granted
+}
+
+
+fn allowed_in_nonsecure_contexts(permission_name: PermissionName) -> bool {
+    match permission_name {
+        // https://w3c.github.io/permissions/#geolocation
+        PermissionName::Geolocation => true,
+        // https://w3c.github.io/permissions/#notifications
+        PermissionName::Notifications => true,
+        // https://w3c.github.io/permissions/#push
+        PermissionName::Push => false,
+        // https://w3c.github.io/permissions/#midi
+        PermissionName::Midi => true,
+        // https://w3c.github.io/permissions/#media-devices
+        PermissionName::Camera => false,
+        PermissionName::Microphone => false,
+        PermissionName::Speaker => false,
+        PermissionName::Device_info => false,
+        // https://w3c.github.io/permissions/#background-sync
+        PermissionName::Background_sync => false,
+        // https://webbluetoothcg.github.io/web-bluetooth/#request-bluetooth-devices Step 4.
+        PermissionName::Bluetooth => false,
+        // https://storage.spec.whatwg.org/#dom-permissionname-persistent-storage
+        PermissionName::Persistent_storage => false,
+    }
+}
+
+impl HeapSizeOf for PermissionDescriptor {
+    fn heap_size_of_children(&self) -> usize {
+        self.name.heap_size_of_children()
+    }
+}
+
+#[allow(unsafe_code)]
+unsafe impl JSTraceable for PermissionDescriptor {
+    unsafe fn trace(&self, trc: *mut JSTracer) {
+        self.name.trace(trc);
+    }
+}
+
+impl PermissionStatusMethods for PermissionStatus {
+    // https://w3c.github.io/permissions/#dom-permissionstatus-state
+    fn State(&self) -> PermissionState {
+        self.state.borrow().clone()
+    }
+
+    // https://w3c.github.io/permissions/#dom-permissionstatus-onchange
+     event_handler!(onchange, GetOnchange, SetOnchange);
+}
